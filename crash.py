1d9b6352abe2ad2b17c73c77ead0ccfb7b3ec67e
ed <ikefire@qq.com>
2020-06-12 16:56:13 +0800

    crash查看

---
+++ crash.py
@@ -0,0 +1,22 @@
+{
+    "os_name": "android",
+    "soft_version": "3.2.2.000.0816.1558",
+    "cuid": "******",
+    "item": {
+        "content": {
+            "type": "1",
+            "skinid": "19570"
+        },
+        "id": "227",
+        "timestamp": "1521716738281",#用户操作发生时间戳
+        "type": "0"
+    },
+    "id": 127, #操作id标示
+    "manufacturer": "xiaomi", #机型
+    "os_version": "7.1.1",
+    "net_type": "wifi",
+    "province": "北京",
+    "city": "beijing"
+}
+
+# 查看bugly -- 崩溃分析 -- 查看标签找到包含对应的灰度版本 -- 点开 -- 找到对应的上面的数据 --查看出错堆栈和跟踪数据
